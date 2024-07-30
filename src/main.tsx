151368821ce33c82d41ea9ee49794e691d6fa415
leandro ruel <proflrr@hotmail.com>
2024-07-30 19:13:58 -0300

    chore: change provider order

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RelayEnvironmentProvider environment={RelayEnvironment}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
       <RouterProvider router={router} />
-    </React.StrictMode>
-  </RelayEnvironmentProvider>
+    </RelayEnvironmentProvider>
+  </React.StrictMode>
 );
