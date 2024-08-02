a38b057690505749f74be8b3530fe85a6416de71
leandro ruel <proflrr@hotmail.com>
2024-08-02 12:01:42 -0300

    chore: Update router configuration with error handling for login and register pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,29 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../Login";
 import Register from "../Register";
+import ErrorBoundary from "@/ErrorBoundary";
+import PageNotFound from "@/PageNotFound";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        
-    },
-    {
-        path: '/login',
-        element: <Login />
-    },
-    {
-        path: '/register',
-        element: <Register />
-    },
-    {
-        path: '*',
-        element: <div>404</div>
-    }
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorBoundary />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <ErrorBoundary />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
 ]);
 
 export default router;
